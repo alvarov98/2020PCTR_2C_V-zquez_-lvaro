93696b7384889437c4863bd1e8be2f103e340242
alvarov98 <57129525+alvarov98@users.noreply.github.com>
2020-06-14 14:06:27 +0200

    Añadida clase Juego

---
+++ src/simulador_juego/solucion/Juego.java
@@ -0,0 +1,45 @@
+package simulador_juego.enunciado;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class Juego implements IJuego{
+	
+	/* 
+	 *  Definir atributos del juego
+	 * 	
+	 */
+	
+	/*
+	 *  Definir el constructor de la clase con sus atributos.
+	 */
+	public Juego(int maxenemigos, int tiposEnemigos) {
+		
+	}
+
+	/*
+	 *    Implementar la generación de enemigos (con sus condiciones)
+	 * 		La línea en la que se define el método es una plantilla orientativa y puede ser modificada.
+	 */
+	@Override
+	public void generarEnemigo(int tipoEnemigo){
+				
+	}
+	
+	/*
+	 *    Implementar la eliminación de enemigos (con sus condiciones)
+	 * 		La línea en la que se define el método es una plantilla orientativa y puede ser modificada.
+	 */
+	@Override
+	public void eliminarEnemigo(int tipoEnemigo){
+				
+	}
+	
+	/*
+	 *    Implementar el método para mostrar el estado del nivel en cada generación o eliminación de enemigos.
+	 */
+	private void imprimirInfo (int tipoEnemigo, String movimiento){
+		
+	}
+	
+	
