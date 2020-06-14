89f6b3373545499841c8059aa2950b99a0b573c1
alvarov98 <57129525+alvarov98@users.noreply.github.com>
2020-06-14 16:55:11 +0200

    Corregido método run

--- src/simulador_juego/solucion/ActividadEnemiga.java
+++ src/simulador_juego/solucion/ActividadEnemiga.java
@@ -1,35 +1,30 @@
 package simulador_juego.solucion;
-/*
- * @author Álvaro Vázquez Suárez
- * @date 14/06/2020
- * @name Práctica Programación Concurrente 2C
- */
+
 import java.util.concurrent.TimeUnit;
+/*
+ * @author <a href="mailto:xvs1001@alu.ubu.es">Álvaro Vázquez</a>
+ */
 
+//DONE Implementar ActividadEnemiga
 public class ActividadEnemiga implements Runnable{
-	
-	//Definir los atributos de la clase
-	
-	private int tipoEnemigo;
-	private IJuego juego;
-	
-	//Implementar el constructor de la clase con sus atributos
-	
-	public ActividadEnemiga(int tipoEnemigo,IJuego juego) {
-		this.tipoEnemigo=tipoEnemigo;
-		this.juego=juego;
-	}
-	
-	//Implementar la actividad aliada (eliminar un enemigo de X tipo)
-	
-	public void run() {
-		juego.generarEnemigo(tipoEnemigo);
-		try {
-			TimeUnit.MILLISECONDS.sleep((long) (Math.random()*5));
-		}catch(InterruptedException e){
-			e.printStackTrace();
-		}
-	}
-	
+		
+		// DONE Definir los atributos de la clase
 
+		private int tipoEnemigo;
+		private IJuego juego;  
+		// DONE Implementar el constructor de la clase con sus atributos
+		public ActividadEnemiga(int tipoEnemigo,IJuego juego) {
+			this.tipoEnemigo=tipoEnemigo;
+			this.juego=juego;
+		}
+		
+		// DONE Implementar la actividad enemiga (generar un enemigo de X tipo y esperar X tiempo)
+		public void run() {
+			juego.generarEnemigo(tipoEnemigo);
+			try {
+				TimeUnit.MILLISECONDS.sleep((long) (Math.random()*5));
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
 }
