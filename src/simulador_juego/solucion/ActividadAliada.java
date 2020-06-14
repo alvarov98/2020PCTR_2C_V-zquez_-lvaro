8d4f69f5c15ebb1c0aae6835fa948223fe5b74b1
alvarov98 <57129525+alvarov98@users.noreply.github.com>
2020-06-14 16:54:43 +0200

    Corregido método run

--- src/simulador_juego/solucion/ActividadAliada.java
+++ src/simulador_juego/solucion/ActividadAliada.java
@@ -1,33 +1,36 @@
 package simulador_juego.solucion;
-/*
- * @author Álvaro Vázquez Suárez
- * @date 14/06/2020
- * @name Práctica Programación Concurrente 2C
+
+/**
+ * Clase ActividadAliada
+ * @author <a href="mailto:xvs1001@alu.ubu.es">Álvaro Vázquez</a>
+ *
  */
 import java.util.concurrent.TimeUnit;
+//DONE Implementar ActividadAliada
+public class ActividadAliada implements Runnable{
+		
+		// DONE Definir los atributos de la clase
+		private int tipoEnemigo;
+		private IJuego juego;
 
-public class ActividadAliada implements Runnable {
-
-	//Definir los atributos de la clase
-
-	private int tipoEnemigo;
-	private IJuego juego;
-
-	//Implementar el constructor de la clase con sus atributos
-
-	public ActividadAliada(int tipoEnemigo, IJuego juego) {
-		this.tipoEnemigo = tipoEnemigo;
-		this.juego = juego;
-	}
-
-	//Implementar la actividad aliada (eliminar un enemigo de X tipo)
-
-	public void run() {
-		juego.eliminarEnemigo(tipoEnemigo);
-		try {
-			TimeUnit.MILLISECONDS.sleep((long) (Math.random()*5));
-		}catch(InterruptedException e){
-			e.printStackTrace();
+		
+		// DONE Implementar el constructor de la clase con sus atributos
+		public ActividadAliada(int tipoEnemigo, IJuego juego) {
+			this.tipoEnemigo = tipoEnemigo;
+			this.juego = juego;
 		}
-	}
+		// DONE Implementar la actividad aliada (eliminar un enemigo de X tipo)
+		public void run() {
+			try {
+				juego.eliminarEnemigo(tipoEnemigo);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				TimeUnit.MILLISECONDS.sleep((long) (Math.random()*5));
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
 }
