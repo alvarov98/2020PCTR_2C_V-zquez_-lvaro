6c1b374aeb3945b0b72149d22f588d3a2ad276cd
alvarov98 <57129525+alvarov98@users.noreply.github.com>
2020-06-14 16:57:41 +0200

    Corregido inicialización juego y creación hilos

--- src/simulador_juego/solucion/SistemaLanzador.java
+++ src/simulador_juego/solucion/SistemaLanzador.java
@@ -1,23 +1,48 @@
-package simulador_juego.enunciado;
+package simulador_juego.solucion;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Random;
+/**
+ * Clase del sistema lanzador del juego
+ * 
+ * @author <a href="mailto:xvs1001@alu.ubu.es">Álvaro Vázquez</a>
+ */
 
 public class SistemaLanzador {
-	public static void main(String[] args) {
-		
-		/* 
-		 *    Crear un diccionario con la estrutura: 
-		 * 		Clave - tipo de enemigo
-		 * 		Valor - número de enemigos de ese tipo que se deben generar
-		 */
-		
-		
-		/* 
-		 *     Inicializar juego
-		 */
-		
-		
-		/* 
-		 *     Crear los hilos para la actividad aliada y enemiga (por pares) iterando sobre el diccionario anterior.
-		 */
+
+		public static void main(String[] args) {
+			int maxEnemigos = 20;
+			int tiposEnemigos = 4;
 			
-	}	
+			/* 
+			 * DONE Crear un diccionario con la estrutura: 
+			 * 		Clave - tipo de enemigo
+			 * 		Valor - número de enemigos de ese tipo que se deben generar
+			 */
+			Map<Integer,Integer> enemigos = new HashMap<>();
+			
+			for (int tipo = 0; tipo < tiposEnemigos; tipo++) {
+				enemigos.put(tipo,0);
+			}
+			/* 
+			 * DONE Inicializar juego
+			 */
+			IJuego juego= new Juego(maxEnemigos, tiposEnemigos);
+			
+
+			/* 
+			 * DONE Crear los hilos para la actividad aliada y enemiga (por pares) iterando sobre el diccionario anterior.
+			 */
+			
+				for(int i=0; i<tiposEnemigos - 1; i++) {
+					for(int j = 0; j< enemigos.size(); j++){
+					Thread eliminar =new Thread(new ActividadAliada(i, juego));
+					Thread generar = new Thread(new ActividadEnemiga(i,juego));
+					generar.start();
+					eliminar.start();
+				}
+			}
+		}	
+
 }
