f9afcd7d593e2e587e0572310b85da744eaaf853
alvarov98 <57129525+alvarov98@users.noreply.github.com>
2020-06-14 12:25:33 +0200

    Creada clase ActividadAliada
    Añadidos atributos de clase, constructor con atributos e implementación del metodo Run

--- src/simulador_juego/solucion/ActividadAliada.java
+++ src/simulador_juego/solucion/ActividadAliada.java
@@ -1,1 +1,33 @@
+package simulador_juego.solucion;
+/*
+ * @author Álvaro Vázquez Suárez
+ * @date 14/06/2020
+ * @name Práctica Programación Concurrente 2C
+ */
+import java.util.concurrent.TimeUnit;
 
+public class ActividadAliada implements Runnable {
+
+	//Definir los atributos de la clase
+
+	private int tipoEnemigo;
+	private IJuego juego;
+
+	//Implementar el constructor de la clase con sus atributos
+
+	public ActividadAliada(int tipoEnemigo, IJuego juego) {
+		this.tipoEnemigo = tipoEnemigo;
+		this.juego = juego;
+	}
+
+	//Implementar la actividad aliada (eliminar un enemigo de X tipo)
+
+	public void run() {
+		juego.eliminarEnemigo(tipoEnemigo);
+		try {
+			TimeUnit.MILLISECONDS.sleep((long) (Math.random()*5));
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
